a578bbe66c00de79d5483e3b6c2a28d072095c65
Gwon Seonggwang <g.passcode@gmail.com>
2020-05-24 17:05:32 +0900

    finish 3.0.0

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -21,6 +20,17 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        let mut iter = s.split(",");
+        match (
+            iter.next(),
+            iter.next().and_then(|v| v.parse::<usize>().ok()),
+        ) {
+            (Some(name), Some(age)) => Ok(Person {
+                name: String::from(name),
+                age,
+            }),
+            _ => Err(String::from("Malformed input")),
+        }
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -2,7 +2,7 @@
 // Basically, this is the same as From. The main difference is that this should return a Result type
 // instead of the target type itself.
 // You can read more about it at https://doc.rust-lang.org/std/convert/trait.TryFrom.html
-use std::convert::{TryInto, TryFrom};
+use std::convert::{TryFrom, TryInto};
 
 #[derive(Debug)]
 struct Person {
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Your task is to complete this implementation
 // in order for the line `let p = Person::try_from("Mark,20")` to compile
 // and return an Ok result of inner type Person.
@@ -28,6 +27,17 @@ struct Person {
 impl TryFrom<&str> for Person {
     type Error = String;
     fn try_from(s: &str) -> Result<Self, Self::Error> {
+        let mut iter = s.split(",");
+        match (
+            iter.next(),
+            iter.next().and_then(|v| v.parse::<usize>().ok()),
+        ) {
+            (Some(name), Some(age)) => Ok(Person {
+                name: String::from(name),
+                age,
+            }),
+            _ => Err(String::from("Malformed input")),
+        }
     }
 }
 
